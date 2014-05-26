022e8b1db32477f79af32111c2a0aa95804f90f9
remasik <remasik@gmail.com>
2014-05-26 17:48:53 +0400

    some fix

--- pytrans.py
+++ pytrans.py
@@ -3,8 +3,8 @@ from tkinter import *
 import os
 import sys
 import getopt
-import requests
 import re
+#import requests
 tk = Tk()
 tk.title("pytrans")
 tk.overrideredirect(True)
@@ -25,14 +25,15 @@ def help():
 	-a\tSettings.
 	-h\tPrint help.""")
 	close()
-def translate(arg):
+def translate():
 	transtext = os.popen("xsel -o | sed 's/\"[<>]//g; s/\\./,/g'").read()
-	enc = re.search('[йцукенгшщзхъфывапролджэячсмитьбюё]',transtext) and 'en' or 'ru' 
-	os.system("notify-send 'google''"+requests.request("GET", "http://translate.google.com/translate_a/t?client=t&text="+transtext+"&sl=auto&tl="+enc, headers={'User-Agent': 'Mozilla/5.0'}).transtext.split("\"")[1]+ "")
-	close()
+	Label(tk, text=transtext, bg="#000000",fg="#e5e5e5", font=("Terminus", 8)).place(x=0, y=15)
+	#enc = re.search('[йцукенгшщзхъфывапролджэячсмитьбюё]',transtext) and 'en' or 'ru' 
+	#os.system("notify-send 'google''"+requests.request("GET", "http://translate.google.com/translate_a/t?client=t&text="+transtext+"&sl=auto&tl="+enc, headers={'User-Agent': 'Mozilla/5.0'}).transtext.split("\"")[1]+ "")
+	#close()
 def main(argv):
 	try:
-		options, args = getopt.getopt(argv, "a:h:t:")
+		options, args = getopt.getopt(argv, "a:h:t")
 	except getopt.GetoptError:
 		help()
 	for option, arg in options:
@@ -41,10 +42,10 @@ def main(argv):
 		elif option in "-a":
 			comm = arg
 		elif option in "-t":
-			translate(arg)
+			translate()
 #code
 main(sys.argv[1:])
-Label(tk, text="pytrans v.1.0", bg="#0088cc", fg="#e5e5e5", font=("Terminus", 8)).pack(fill=BOTH) 
+Label(tk, text="pytrans v.1.0", bg="#0088cc", fg="#e5e5e5", font=("Terminus", 8)).place(x=0, y=0, width=450) 
 Button(tk, text="close",    command=close, bg="#ff0000", fg="#e5e5e5", font=("Terminus", 8)).place(x = 450, y = 0, height = 14)
 #end
 tk.mainloop()
