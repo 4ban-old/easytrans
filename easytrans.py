1e3736ab8e344a15a2c4e88da3fe0a5c335668c6
Remasik <remasik@gmail.com>
2015-10-27 13:52:18 +0300

    First step of modification easytrans. NOT FINISHED

--- easytrans.py
+++ easytrans.py
@@ -1,91 +1,130 @@
-#!/bin/env python
+# coding: utf-8
 from tkinter import *
-import requests,os,re,sys,configparser
+import requests
+import os
+import re
+import sys
+import configparser
 from urllib.parse import quote
 from multiprocessing import Process, Queue
-tk = Tk()
-tk.title("easytrans")
-tk.overrideredirect(True)
-tk.tkraise()
-tk.grab_set
-tk.minsize(500,500)
-tk.geometry('+500+200')
-config=configparser.ConfigParser()
-config.read('/home/rem/dev/python/easytrans/easytrans.conf')
-panelbackgroundcolor=config['appearance']['panelbgcolor']
-panelforegroundcolor=config['appearance']['panelfgcolor']
-backgroundcolor=config['appearance']['bgcolor']
-foregroundcolor=config['appearance']['fgcolor']
-textfont=config['appearance']['textfont']
-textsize=config['appearance']['textsize']
-#yandex translate api key
-YandexApiKey="trnsl.1.1.20141003T022807Z.4bec480b9480a548.3654b59140bca4ecd7f4d452734d93d8cd526cc3"
 
-def close():
-	tk.quit()
-	os._exit(0)
-def help():
-	print("""Usage:
-	easytrans [-h, -a]\n
-	-a\tSettings.
-	-h\tPrint help.""")
-	close()
-#simple russian language detect
-def detect_lang(text):
-    return re.search('[а-я]', text) and 'en' or 'ru'
-def get_clip():
-    with os.popen("xsel -o") as clip:
-        return clip.read()
-#translate with google translate API
-def goo_translate(q, text, enc):
-    try:
-        resp = requests.request("GET",
-        "http://translate.google.com/translate_a/t?client=x&text=" + quote(text)
-        + "&sl=auto&tl=" + enc, headers={'User-Agent': 'Mozilla/5.0'})
-    except requests.exceptions.ConnectionError:
-        q.put("connection problems")
+class Easytrans:
+    def __init__(self, config):
+        self.config = config
+        que = Queue()
+        clip = get_clip()
+        enc = detect_lang(clip)
+        if len(clip) < 1: _exit()
+        if self.config['yandexapikey'] != '':
+            ya = Process(target=yandex_translate, args=(que, clip, enc,))
+            ya.start()
+        goo = Process(target=google_translate, args=(que, clip, enc,))
+        goo.start()
 
-    if resp.ok:
-        ret = ''
-        for i in resp.json()['sentences']:
-            ret += i['trans']
-        resp.close()
-        print("goo первый!")
-        q.put(ret)
-    else:
-        q.put("connection problems")
+        trans = que.get()
+        if self.config['yandexapikey'] != '':
+            ya.terminate()
+        goo.terminate()
 
-#translate with yandex translate API, for working need API key
-def ya_translate(q, text, enc):
-    try:
-        resp = requests.request("GET",
-        "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" + YandexApiKey
-        + "&text=" + quote(text) + "&lang=" + enc + "&options=1")
-    except requests.exceptions.ConnectionError:
-        q.put("connection problems")
+        Label(root, text="easytrans", font=("textfont", self.config['textsize']), bg=panelbackgroundcolor, fg=panelforegroundcolor).pack(fill='both')
+        Button(root, text="[x]", command=close, font=("textfont", textsize), bg=panelbackgroundcolor, fg=panelforegroundcolor).place(height = 14, width = 25, x=478)
+        translate=Message(tk, text=trans,anchor="nw",padx=10, width="490", font=("textfont", textsize), bg=backgroundcolor, fg=foregroundcolor).pack(side=LEFT, fill=BOTH)
+        Scrollbar(translate).pack(side=RIGHT, fill=Y)
 
-    if resp.ok:
-        resp.close()
-        print("ya закончил!")
-        q.put(resp.json()['text'][0])
-    else:
-        q.put("yandex "+ resp.json()['message'])
+    def _exit(self):
+        root.destroy()
+        sys.exit()
+        root.quit()
+        os._exit(0)
+
+    def _help(self):
+        """
+            Print help into terminal.
+        """
+        print """Usage:
+        easytrans [-h, -a]\n
+        -a\tSettings.
+        -h\tHelp."""
+
+    def read_config(self):
+        """
+            Reading config file.
+        :param config: configparser
+        :return: dictionary with results.
+        """
+        res_config = dict()
+        config=configparser.ConfigParser()
+        home = os.path.expanduser("~")
+        config.read(home+'.dop/easytrans/easytrans.conf')
+        res_config['panelbackgroundcolor']=config['appearance']['panelbgcolor']
+        res_config['panelforegroundcolor']=config['appearance']['panelfgcolor']
+        res_config['backgroundcolor']=config['appearance']['bgcolor']
+        res_config['foregroundcolor']=config['appearance']['fgcolor']
+        res_config['textfont']=config['appearance']['textfont']
+        res_config['textsize']=config['appearance']['textsize']
+        res_config['overridedirect']=config['settings']['overridedirect']
+        res_config['win_mode']=config['settings']['win_mode']
+        res_config['yandexapikey']=config['yandexapi']['yandexapikey']
+        return res_config
+
+    def detect_lang(self, text):
+        return re.search('[а-я]', text) and 'en' or 'ru'
+
+    def get_clip(self):
+        with os.popen("xsel -o") as clip:
+            return clip.read()
+
+    def google_translate(self, q, text, enc):
+        try:
+            resp = requests.request("GET",
+            "http://translate.google.com/translate_a/t?client=x&text=" + quote(text)
+            + "&sl=auto&tl=" + enc, headers={'User-Agent': 'Mozilla/5.0'})
+        except requests.exceptions.ConnectionError:
+            q.put("connection problems")
+
+        if resp.ok:
+            ret = ''
+            for i in resp.json()['sentences']:
+                ret += i['trans']
+            resp.close()
+            print("goo первый!")
+            q.put(ret)
+        else:
+            q.put("connection problems")
+
+    #translate with yandex translate API, for working need API key
+    def yandex_translate(self, q, text, enc):
+        try:
+            resp = requests.request("GET",
+            "https://translate.yandex.net/api/v1.5/tr.json/translate?key=" + YandexApiKey
+            + "&text=" + quote(text) + "&lang=" + enc + "&options=1")
+        except requests.exceptions.ConnectionError:
+            q.put("connection problems")
+
+        if resp.ok:
+            resp.close()
+            print("ya закончил!")
+            q.put(resp.json()['text'][0])
+        else:
+            q.put("yandex "+ resp.json()['message'])
 
 if __name__ == "__main__":
-	que=Queue()
-	clip=get_clip()
-	enc=detect_lang(clip)
-	if len(clip) < 1: sys.exit(0)
-	if YandexApiKey != '': ya = Process(target=ya_translate, args=(que, clip, enc,))
-	goo = Process(target=goo_translate, args=(que, clip, enc,))
-	if YandexApiKey != '': ya.start()
-	goo.start()
-	#get return
-	trans = que.get()
-	if YandexApiKey != '': ya.terminate()
-	goo.terminate()
-	Label(tk, text="easytrans", font=("textfont", textsize), bg=panelbackgroundcolor, fg=panelforegroundcolor).pack(fill='both')
-	Button(tk, text="[x]", command=close, font=("textfont", textsize), bg=panelbackgroundcolor, fg=panelforegroundcolor).place(height = 14, width = 25, x=478)
-	translate=Message(tk, text=trans,anchor="nw",padx=10, width="490", font=("textfont", textsize), bg=backgroundcolor, fg=foregroundcolor).pack(side=LEFT, fill=BOTH)
-	Scrollbar(translate).pack(side=RIGHT, fill=Y)
-	tk.mainloop()
+    root = Tk()
+    root.title("easytrans v.1.0")
+    config = Easytrans.read_config()
+    if config['overridedirect']:
+        root.overrideredirect(True)
+    root.tkraise()
+    root.grab_set
+    # notification mode: True-windows, False - notify daemon
+    mode = True
+    if not config['win_mode']:
+        mode = config['win_mode']
+
+    #x = (root.winfo_screenwidth() - root.winfo_reqwidth()) / 2
+    #y = (root.winfo_screenheight() - root.winfo_reqheight()) / 2
+    #root.wm_geometry("+%d+%d" % (x, y))
+    #root.minsize(500,500)
+    #root.geometry('+500+200')
+    Easytrans(config)
+    root.mainloop()
